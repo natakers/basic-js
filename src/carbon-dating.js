8563d2403ea90a37473841574ddf17830f13721e
natakers <n.kersnovskaya@gmail.com>
2021-09-12 15:45:20 +0300

    feat: add code carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,8 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+export default function dateSample(sampleActivity) {
+  // throw new NotImplementedError('Not implemented');
+  return Math.ceil(Math.log(MODERN_ACTIVITY/Number(sampleActivity))*0.693/HALF_LIFE_PERIOD);
   // remove line with error and write your code here
 }
