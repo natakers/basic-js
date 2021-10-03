026723f0797b7e24887d9ecb95f187d9fbbd2347
natakers <n.kersnovskaya@gmail.com>
2021-10-03 17:33:13 +0300

    add code

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,58 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+export default function repeater(str, options) {
+  let str1 = '';
+  let str2 = '';
+  // return ((str +  (options.additionSeparator + options.addition).repeat(options.additionRepeatTime) + options.separator).repeat(options.repeatTimes));
+  // return (options.additionSeparator + options.addition).repeat(options.additionRepeatTimes) ;
+  // return options.separator;
+  // separatorзначение по умолчанию '+'. additionSeparatorзначение по умолчанию '|'.
+  // if ()
+
+  if (options.addition) {
+  if (options.additionRepeatTimes) {
+    if (options.additionSeparator) {
+      str1 = (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes).substring(0, (str1.length-options.additionSeparator.length));
+    }
+    else str1 = (options.addition).repeat(options.additionRepeatTimes);
+  }
+  else str1 = options.addition;
+}
+else str1 = '';
+
+  if (options.repeatTimes) {
+    if (options.repeatTimes == 1) {
+      options[separator] = '+';
+    }
+    if (options.separator) {
+      str = (str + str1 + options.separator).repeat(options.repeatTimes)
+      str =  str.substring(0, ((str.length)-(options.separator.length)));
+    }
+    else {
+      if (options.repeatTimes == 1) {
+        str = (str + str1 + "+").repeat(options.repeatTimes)
+    str = str.substring(0, ((str.length)-1));
+      }
+      
+    }
+     
+  }
+  else str = str + str1;
+
+  // return [str, str.length];
+  return str;
+
+
+
+
+
+//   repeatTimesустанавливает набор numberповторов str;
+// separatorэто stringразительное повторение str;
+// additionявляется дополнительным, stringкоторое будет добавляться к каждому повторению str;
+// additionRepeatTimesустанавливает набор numberповторов addition;
+// additionSeparator
+  // console.log 
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,8 +13,28 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
+  calculateDepth(arr) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    let count = 0;
+    count1(arr);
+  //   if (a.length == 0 ) {return 0;}
+    function count1(a){
+      console.log(a);
+      count = count++;
+    a.forEach(item => {
+      console.log('count ' + count);
+      console.log(item);
+      if (Array.isArray(item)) {
+      var b = item;
+      return count1(b);
+      }
+    }); 
+    return count;
+    }
+     console.log('count ' + count);
+    return count;
   }
 }
+
+// calculateDepth([1, 2, 3, 4, 5, [1]])
